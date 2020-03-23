5b3045d207f0d22e3e60856134c3dbde74391627
rezaduha <rezafauzizisiwaduha@gmail.com>
2020-03-23 16:46:33 +0700

    fix url newsapi

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`http://newsapi.org/v2/everything?q=corona&country=id&apiKey=${process.env.NEWS_API_KEY}`);
+      const response = await axios.get(`http://newsapi.org/v2/top-headlines?q=corona&country=id&apiKey=${process.env.NEWS_API_KEY}`);
       return response.data;
     } catch (error) {
       console.error(error);
